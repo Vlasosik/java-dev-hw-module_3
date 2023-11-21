96b239cc089ee44e1c13877012a5783d8fad5920
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-11-21 21:56:58 +0200

    creating tests according to the homework

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SumCalculator {
+
+    public int sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+}
---
+++ src/test/java/SumCalculatorTests.java
@@ -0,0 +1,33 @@
+import org.example.SumCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SumCalculatorTests {
+
+    private SumCalculator calculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        calculator = new SumCalculator();
+    }
+
+    @Test
+    void calculatorSumNumber1Test() {
+        int actual = calculator.sum(1);
+        int expected = 1;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void calculatorSumNumber3Test() {
+        int actual = calculator.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(actual, expected);
+    }
+
+        @Test
+    void testExpectedException() {
+       Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.sum(0));
+    }
+}
